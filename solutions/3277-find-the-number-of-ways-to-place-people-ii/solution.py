41aae007b8c2e305d9e240d7ab172d765efa0472
Ackrome <ivantishchenko59@gmail.com>
2025-09-03 08:01:21 +0000

    [LeetCode Sync] Runtime - 8264 ms (5.01%), Memory - 18.4 MB (44.72%)

---
+++ solutions/3277-find-the-number-of-ways-to-place-people-ii/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numberOfPairs(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda p: (p[0], -p[1]))
+        print(points)
+        n = len(points)
+        count = 0
+
+
+        for i in range(n):
+            for j in range(i+1,n):
+                if points[i][1]<points[j][1]:
+                    continue
+
+                valid = True
+                for k in range(i+1,j):
+                    if points[j][1]<=points[k][1]<=points[i][1]:
+                        valid = False
+                        break
+                
+                if valid:
+                    print( points[i], points[j])
+
+                    count+=1
+        return count
