a9d117a40d7d2854c3779dd903656f4a7ff49b1f
Ackrome <ivantishchenko59@gmail.com>
2025-10-31 06:25:02 +0000

    [LeetCode Sync] Runtime - 112 ms (79.05%), Memory - 18.1 MB (49.20%)

---
+++ solutions/0424-longest-repeating-character-replacement/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = defaultdict(int)
+        max_freq, left, max_length = 0,0,0
+
+        for right in range(len(s)):
+            count[s[right]] += 1
+            max_freq = max(max_freq, count[s[right]])
+
+            if (right - left + 1) - max_freq > k:
+                count[s[left]] -= 1
+                left += 1
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
